5c992fce0b70285052b9194ac3885dd3e13ac3d2
ilikestreet <ilikestreet.jz@gmail.com>
2017-05-22 13:48:49 -0700

    Add files via upload

---
+++ myApt/LogCollectionViewController.swift
@@ -0,0 +1,151 @@
+//
+//  LogCollectionViewController.swift
+//  myHome
+//
+//  Created by Johnny Zhang on 2/12/17.
+//  Copyright © 2017 Johnny Zhang. All rights reserved.
+//
+
+import UIKit
+
+private let cellID = "Cell"
+
+class LogCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout{
+    
+    
+    
+    public var commandArray = [String]()
+    
+
+   
+    func handleRightEdgeSwipe(gesture: UIScreenEdgePanGestureRecognizer) {
+        if gesture.state == .recognized{
+            let transition = CATransition()
+            transition.duration = 0.5
+            transition.type = kCATransitionFade
+            transition.subtype = kCATransitionFromRight
+            self.navigationController?.view.layer.add(transition, forKey: nil)
+            let _ = self.navigationController?.popToRootViewController(animated: false)
+            print("right swipped")
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+
+        self.view.layoutIfNeeded()
+        let item = self.collectionView(self.collectionView!, numberOfItemsInSection: 0) - 1
+        if item > 0 {
+            let lastItemIndex = NSIndexPath(item: item, section: 0) as IndexPath
+            self.collectionView?.scrollToItem(at: lastItemIndex, at: UICollectionViewScrollPosition.bottom, animated: false)
+        }
+        UIApplication.shared.statusBarStyle = .lightContent
+    }
+    
+    
+    override func willRotate(to toInterfaceOrientation: UIInterfaceOrientation, duration: TimeInterval) {
+        collectionViewLayout.invalidateLayout()
+    }
+    
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        let rightEdgeSwipe = UIScreenEdgePanGestureRecognizer(target: self, action: #selector(handleRightEdgeSwipe(gesture:)))
+        rightEdgeSwipe.edges = .right
+        view.addGestureRecognizer(rightEdgeSwipe)
+        
+        
+        collectionView?.backgroundColor = UIColor.black
+        self.collectionView!.register(LogCell.self, forCellWithReuseIdentifier: cellID)        
+    }
+    
+    
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return commandArray.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath) as! LogCell
+        
+        // Configure the cell
+        
+        cell.logTextView.text = commandArray[indexPath.row]
+        
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let commandText = commandArray[indexPath.row]
+        if commandText != "" {
+            let size = CGSize(width: view.frame.width, height: 1000)
+            let options = NSStringDrawingOptions.usesFontLeading.union(.usesLineFragmentOrigin)
+            
+            let estimatedFrame = NSString(string: commandText).boundingRect(with: size, options: options, attributes: [NSFontAttributeName : UIFont.boldSystemFont(ofSize: 26)], context: nil)
+            
+            return CGSize(width: view.frame.width, height: estimatedFrame.height + 20)
+        }
+        return CGSize(width: view.frame.width, height: 100)
+    }
+    
+}
+
+class LogCell: BaseCell {
+    
+    let logTextView: UITextView = {
+        let textView = UITextView()
+        textView.font = UIFont.boldSystemFont(ofSize: 26)
+        textView.text = ""
+        textView.textColor = UIColor.green
+        textView.backgroundColor = UIColor.clear
+        textView.isEditable = false
+        textView.isScrollEnabled = false
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        return textView
+    }()
+    
+    override func setupView() {
+        backgroundColor = UIColor.clear
+        addSubview(logTextView)
+        setupLogTextViewConstraints()
+        
+    }
+    
+    func setupLogTextViewConstraints() {
+        addConstraintsWithFormat(format: "H:|[v0]|", views: logTextView)
+        addConstraintsWithFormat(format: "V:|[v0]|", views: logTextView)
+    }
+}
+
+class BaseCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupView() {}
+}
+
+extension UIView{
+    func addConstraintsWithFormat(format: String, views: UIView...)  {
+        var viewDics = [String: UIView]()
+        for (index, uiview) in views.enumerated() {
+            let key = "v\(index)"
+            viewDics[key] = uiview
+        }
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutFormatOptions(), metrics: nil, views: viewDics))
+    }
+}
+
+
+
---
+++ myApt/SwitchViewController.swift
@@ -0,0 +1,291 @@
+//
+//  SwitchViewController.swift
+//  myHome
+//
+//  Created by Johnny Zhang on 2/9/17.
+//  Copyright © 2017 Johnny Zhang. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+import FirebaseDatabase
+
+
+class SwitchViewController: UIViewController {
+    
+    var userID: String!
+    var ref: FIRDatabaseReference!
+    var fanStatus: Int = -1
+    
+    let logController = LogCollectionViewController(collectionViewLayout: UICollectionViewFlowLayout())
+    let customBackground = UIColor(red: 254, green: 255, blue: 237)
+    
+    let contentView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.clear
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 10
+        view.layer.masksToBounds = true
+        return view
+    }()
+    
+    let lightLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Light:"
+        label.textColor = UIColor.black
+//        label.backgroundColor = UIColor.red
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 32)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let fanLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Fan:"
+        label.textColor = UIColor.black
+//        label.backgroundColor = UIColor.red
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 32)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let lightSwitch: UISwitch = {
+       let sw = UISwitch()
+//        sw.backgroundColor = UIColor.red
+        sw.isUserInteractionEnabled = false
+        sw.translatesAutoresizingMaskIntoConstraints = false
+        return sw
+    }()
+    
+    let fanStatusLabel: UILabel = {
+        let label = UILabel()
+        label.text = ""
+        label.textColor = UIColor.black
+//        label.backgroundColor = UIColor.red
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 32)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        getLogFromFirebase()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        UIApplication.shared.statusBarStyle = .default
+    }
+
+    
+   override func viewDidLoad() {
+        super.viewDidLoad()
+    
+
+        contentView.backgroundColor = customBackground
+        view.backgroundColor = customBackground
+    
+        view.addSubview(contentView)
+        contentView.addSubview(lightLabel)
+        contentView.addSubview(fanLabel)
+        contentView.addSubview(lightSwitch)
+        contentView.addSubview(fanStatusLabel)
+        
+        setupContentViewConstraint()
+        setupLightLabelViewConstraint()
+        setupLightSwitchViewConstraint()
+        setupFanLabelViewConstraint()
+        setupFanStatusLabelViewConstraint()
+        fanStatusLabel.text = String(fanStatus)
+
+    
+        setupNetwork()
+        setupLightSwitch()
+        setupGesture()
+
+    }
+    
+    
+    func setupNetwork(){
+        let email = "ilikestreet.jz@gmail.com"
+        let password = "Abc110110119"
+        
+        
+        FIRAuth.auth()?.signIn(withEmail: email, password: password) { (user, error) in
+            //Print into the console if successfully logged in
+            print("You have successfully logged in")
+
+        }
+        
+        userID = FIRAuth.auth()?.currentUser?.uid
+        ref = FIRDatabase.database().reference().child(self.userID)
+        
+        
+
+    }
+
+    func setupLightSwitch() {
+        let checkingAlert = UIAlertController(title: "Checking Status", message: "Please wait", preferredStyle: .alert)
+        self.present(checkingAlert, animated: true, completion: nil)
+        
+        ref.child("lastLightCommand").observe(FIRDataEventType.value, with: { (snapshot) in
+            if let value = snapshot.value as? String {
+                self.lightSwitch.setOn(value == "LIGHT ON", animated: true)
+            }
+        })
+        
+        checkingAlert.dismiss(animated: true, completion: nil)
+    }
+    
+    
+
+        
+    
+    
+    func setupGesture() {
+        let leftSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipeAction))
+        leftSwipe.direction = .left
+        view.addGestureRecognizer(leftSwipe)
+        let rightSwipe = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipeAction))
+        rightSwipe.direction = .right
+        view.addGestureRecognizer(rightSwipe)
+        let oneFingerTap = UITapGestureRecognizer(target: self, action: #selector(handleOneFingerTap))
+        view.addGestureRecognizer(oneFingerTap)
+
+        let leftEdgeSwipe = UIScreenEdgePanGestureRecognizer(target: self, action: #selector(handleSwipeLeftEdge))
+        leftEdgeSwipe.edges = .left
+        view.addGestureRecognizer(leftEdgeSwipe)
+    }
+
+    
+       
+    
+        
+    
+    func setupContentViewConstraint() {
+        // x, y, width, height
+        contentView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        contentView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        contentView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -80).isActive = true
+        contentView.heightAnchor.constraint(equalToConstant: 120).isActive = true
+    }
+    
+    func setupLightLabelViewConstraint() {
+        // x, y, width, height
+        lightLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor).isActive = true
+        lightLabel.topAnchor.constraint(equalTo: contentView.topAnchor).isActive = true
+        lightLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 1/2).isActive = true
+        lightLabel.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 1/2).isActive = true
+    }
+    
+    
+    func setupLightSwitchViewConstraint() {
+        // x, y, width, height
+        lightSwitch.leftAnchor.constraint(equalTo: lightLabel.rightAnchor, constant: 55).isActive = true
+        lightSwitch.topAnchor.constraint(equalTo: lightLabel.topAnchor, constant: 15).isActive = true
+    }
+    
+    func setupFanLabelViewConstraint() {
+        // x, y, width, height
+        fanLabel.leftAnchor.constraint(equalTo: lightLabel.leftAnchor).isActive = true
+        fanLabel.topAnchor.constraint(equalTo: lightLabel.bottomAnchor).isActive = true
+        fanLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 1/2).isActive = true
+        fanLabel.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 1/2).isActive = true
+    }
+    
+    func setupFanStatusLabelViewConstraint() {
+        // x, y, width, height
+        fanStatusLabel.leftAnchor.constraint(equalTo: fanLabel.rightAnchor).isActive = true
+        fanStatusLabel.topAnchor.constraint(equalTo: fanLabel.topAnchor).isActive = true
+        fanStatusLabel.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 1/2).isActive = true
+        fanStatusLabel.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 1/2).isActive = true
+    }
+    
+    func handleSwipeAction(swipe: UISwipeGestureRecognizer) {
+        if swipe.direction == .left && lightSwitch.isOn {
+            //  Light Off
+            ref.updateChildValues(["Command" : "LIGHT OFF"])
+            ref.updateChildValues(["lastLightCommand" : "LIGHT OFF"])
+            lightSwitch.setOn(false, animated: true)
+            print("Light Off")
+        }
+        else if swipe.direction == .right && !lightSwitch.isOn{
+            //  Light On
+            ref.updateChildValues(["Command" : "LIGHT ON"])
+            ref.updateChildValues(["lastLightCommand" : "LIGHT ON"])
+            lightSwitch.setOn(true, animated: true)
+            print("Light On")
+        }
+        
+    }
+    
+    func handleOneFingerTap(sender: UITapGestureRecognizer) {
+        fanStatus += 1
+        switch fanStatus {
+            case 0:
+                ref.updateChildValues(["Command" : "FAN ON/OFF"])
+                print("FAN On/Off")
+                break
+            
+            case 1...6:
+                ref.updateChildValues(["Command" : "FAN SPEED"])
+                print("FAN Speed")
+                break
+            
+            case 7:
+                ref.updateChildValues(["Command" : "FAN TURN"])
+                print("FAN Turn")
+                break
+            
+            default:
+                fanStatus = -1
+        }
+        
+        fanStatusLabel.text = String(fanStatus)
+    }
+    
+    
+    func getLogFromFirebase(){
+
+        ref.child("Log").observeSingleEvent(of: .value, with: { (snapshot) in
+            if let dic = snapshot.value as? NSDictionary {
+                let logString = dic.map{"\($0):\($1)"}.joined(separator: "\n")
+                let dataArray = logString.characters.split { $0 == "\n" }.map(String.init)
+                self.logController.commandArray = dataArray
+                self.logController.collectionView?.reloadData()
+
+                
+            }
+            else {
+                self.logController.commandArray = []
+                print("None")
+            }
+        })
+    }
+    
+    func handleSwipeLeftEdge(gesture: UIScreenEdgePanGestureRecognizer) {
+        if gesture.state == .recognized {
+            let transition = CATransition()
+            transition.duration = 0.5
+            transition.type = kCATransitionFade
+            transition.subtype = kCATransitionFromLeft
+            self.navigationController?.view.layer.add(transition, forKey: nil)
+            self.navigationController?.pushViewController(logController, animated: false)
+            print("left swipped")
+        }
+    }
+
+}
+
+extension UIColor{
+    convenience init(red: Float, green: Float, blue: Float) {
+        self.init(colorLiteralRed: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+}
+
+
